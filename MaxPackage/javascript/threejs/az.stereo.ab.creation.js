8a540ed8d272e3159a2cb8b4b0a0933cdb247151
Nipaleme <adrien.zanni@hotmail.fr>
2023-11-26 14:46:09 +0100

    clean + have only one spat5.osc.route

--- MaxPackage/javascript/threejs/az.stereo.ab.creation.js
+++ MaxPackage/javascript/threejs/az.stereo.ab.creation.js
@@ -1,3 +1,8 @@
+
+// inlets and outlets
+inlets = 1;
+outlets = 1;
+
 var myval = 0;
 
 if (jsarguments.length > 1) myval = jsarguments[1];
@@ -5,9 +10,17 @@ if (jsarguments.length > 1) myval = jsarguments[1];
 var sourceNumber = 1;
 var micNumber = 2;
 
-function routeScript(posX, scriptname, index) {
-  return [scriptname, posX, 200, "spat5.osc.route", "/source/" + String(index)];
+function commonRouteScript(sourceNumber) {
+  const listSources = [200, 133, "spat5.osc.route"];
+  for (i = 0, j = sourceNumber; i < j; i += 1) {
+    listSources.push("/source/" + String(i + 1));
+  }
+  return listSources;
 }
+
+// function routeScript(posX, scriptname, index) {
+//   return [scriptname, posX, 200, "spat5.osc.route", "/source/" + String(index)];
+// }
 
 function routepassScript(posX, scriptname) {
   return [scriptname, posX, 235, "spat5.osc.routepass", "/delays", "/gains"];
@@ -45,44 +58,54 @@ function unpackScript(channel) {
   return [30, 224, "mc.unpack~", channel];
 }
 
+function createOneObj(newAddress, creationArray, customsize) {
+  var newObj = null;
+  newObj = this.patcher.getnamed(newAddress);
+  if (newObj !== null) {
+    this.patcher.remove(newObj);
+  }
+  vbox = this.patcher.newdefault(creationArray);
+  vbox.sendbox("varname", newAddress);
+  vbox.sendbox("style", "redness");
+  vbox.sendbox("hint", newAddress);
+  vbox.sendbox("annotation", newAddress);
+  if (customsize !== undefined) {
+    vbox.sendbox("size", [customsize,15]);
+  }
+
+  newObj = this.patcher.getnamed(newAddress);
+  return newObj;
+}
+
 function createDSP(sourceNumber, micNumber) {
   // create common unpack
-  const unpackAddress = "mc.unpack";
-
-  var unpackObj = null;
-  unpackObj = this.patcher.getnamed(unpackAddress);
-  if (unpackObj !== null) {
-    this.patcher.remove(unpackObj);
-  }
-  vbox = this.patcher.newdefault(unpackScript(sourceNumber));
-  vbox.sendbox("varname", unpackAddress);
-  vbox.sendbox("style", "redness");
-  vbox.sendbox("hint", unpackAddress);
-  vbox.sendbox("annotation", unpackAddress);
-
-  unpackObj = this.patcher.getnamed(unpackAddress);
+  const unpackObj = createOneObj("mc.unpack", unpackScript(sourceNumber));
   const audioInObj = this.patcher.getnamed("audio_in");
   this.patcher.connect(audioInObj, 0, unpackObj, 0);
+
+  //create common route
+  const commonRouteObj = createOneObj(
+    "spat5.osc.route/all",
+    commonRouteScript(sourceNumber),
+    (sourceNumber)*255
+  );
+  const jsObj = this.patcher.getnamed("az_stereo_ab_creation_js");
+  this.patcher.connect(jsObj, 0, commonRouteObj, 0);
 
   //create sources objects
   for (i = 0, j = sourceNumber; i < j; i += 1) {
     const sourceArray = [
-      routeScript(
-        200 + 200 * i,
-        "spat5.osc.route/source/" + String(i + 1),
-        i + 1
-      ),
       routepassScript(
-        200 + 200 * i,
+        200 + 250 * i,
         "spat5.osc.routepass/source/" + String(i + 1)
       ),
       tapeoutScript(
-        200 + 200 * i,
+        200 + 250 * i,
         "spat5.tapeout/source/" + String(i + 1),
         micNumber
       ),
       diagmatrixScript(
-        200 + 200 * i,
+        200 + 250 * i,
         "spat5.diagmatrix/source/" + String(i + 1),
         micNumber
       ),
@@ -90,31 +113,19 @@ function createDSP(sourceNumber, micNumber) {
 
     var previousobj = null;
     for (m = 0, n = sourceArray.length; m < n; m += 1) {
-      var maxobj = null;
-
       const currArray = sourceArray[m];
       const address = currArray.splice(0, 1);
-      maxobj = this.patcher.getnamed(address);
-      if (maxobj !== null) {
-        this.patcher.remove(maxobj);
-      }
-      vbox = this.patcher.newdefault(currArray);
-      vbox.sendbox("varname", address);
-      vbox.sendbox("style", "redness");
-      vbox.sendbox("hint", address);
-      vbox.sendbox("annotation", address);
+      var maxobj = createOneObj(address, currArray);
 
-      maxobj = this.patcher.getnamed(address);
       //connect
       if (m === 0) {
-        const jsObj = this.patcher.getnamed("az_stereo_ab_creation_js");
-        this.patcher.connect(jsObj, 0, maxobj, 0);
+        this.patcher.connect(commonRouteObj, i, maxobj, 0);
       } else {
         this.patcher.connect(previousobj, 0, maxobj, 0);
-        if (m === 2) {
+        if (m === 1) {
           this.patcher.connect(unpackObj, i, maxobj, 0);
         }
-        if (m === 3) {
+        if (m === 2) {
           const routepassObj = this.patcher.getnamed(
             "spat5.osc.routepass/source/" + String(i + 1)
           );
@@ -128,7 +139,6 @@ function createDSP(sourceNumber, micNumber) {
   }
   for (k = sourceNumber, l = 100; k < l; k += 1) {
     const toDeleteArray = [
-      "spat5.osc.route/source/" + String(k + 1),
       "spat5.osc.routepass/source/" + String(k + 1),
       "spat5.tapeout/source/" + String(k + 1),
       "spat5.diagmatrix/source/" + String(k + 1),
@@ -147,7 +157,7 @@ function anything() {
   const a = arrayfromargs(messagename, arguments);
   if (a[0] === "/source/number") {
     post("received message " + a + "\n");
-    if (a[1] !== micNumber) {
+    if (a[1] !== sourceNumber) {
       sourceNumber = a[1];
       createDSP(a[1], micNumber);
     }
